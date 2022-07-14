f9ec8fc048a30e62f672fae27f7f63cc2cbe136b
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:45:55 -0700

    Create quiz_4.py
    Use shuffle, sample from random 
    Use .format()

---
+++ quiz_4.py
@@ -0,0 +1,18 @@
+'''
+There is a python coding competition.
+To increase its participation rate, there will be prizes.
+From the commenters, 1 person will have a Fried Chicken, 3 people will have a free cup of coffee. 
+
+Make a Lottery Program.
+
+Rule 1 - there are 20 commenters and ID is 1~20
+Rule 2 - regardless of contents of comments, it will select randomly and no replication
+Rule 3 - From the random module, use shuffle() and sample()
+
+Example:
+ -- Lottery Winner --
+ Fried Chicken : 1
+ Coffee : [2, 3, 4]
+ -- Congratulations --
+'''
+from random import *
