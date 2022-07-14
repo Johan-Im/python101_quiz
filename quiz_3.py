6dd5506e079cb00e58888253f7cb8ca7bd652b19
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:42:46 -0700

    Create quiz_3.py
    Use slicing/indexing to make a password

---
+++ quiz_3.py
@@ -0,0 +1,11 @@
+'''
+# Make a program that generates a password for each website
+
+# Example: http://naver.com
+# Rule 1 : remove http:// -> naver.com
+# Rule 2 : remove letters after (.) -> naver
+# Rule 3:  Generate as the following: The first 3 letters + number of the letters of word + number of 'e' in the word + "!"
+#                                                (nav)                   (5)                              (1)                (!)
+
+# Example Answer: nav51!                                                
+''' 
