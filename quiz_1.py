49cb3dadf5ba1e5feea256fec278939b1a2447b4
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:37:22 -0700

    Create quiz_1.py
    Utilize given strings and print integers as strings

---
+++ quiz_1.py
@@ -0,0 +1,11 @@
+'''
+By using the information below, introduce your pet! 
+For instance, "My pet is a ____, and his name is ____..."
+And finally show whether your pet is an Adult or not (True / False) by using "is_adult" information. 
+'''
+
+animal = 
+name = 
+age = 
+hobby = 
+is_adult = age >= 1 
