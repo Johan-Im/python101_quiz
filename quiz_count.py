7a99cf8ecc6cfb48bd2ce62b955a3f244b6ad5c3
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:57:54 -0700

    Create quiz_count.py
    count using for loop and set

---
+++ quiz_count.py
@@ -0,0 +1,8 @@
+'''
+Count how many fruits in the "fruit" list
+
+Example:
+{'apple': 2, 'banana': 2, 'strawberry': 1, 'kiwi': 1, 'peach': 3}
+'''
+
+fruit = ["apple", "apple", "banana", "banana", "strawberry", "kiwi", "peach", "peach","peach"]
