a049fd59c84e890cc06f38413ab6683335bd9f72
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:52:14 -0700

    Create quiz_7.py
    with open()

---
+++ quiz_7.py
@@ -0,0 +1,16 @@
+''' At your Company, you need to wrtie a report "once" a week
+The report needs to be as below:
+
+- Week xx weekly report -
+Department:
+Name:
+Work Summary:
+
+Make a program that reports a file from week 1 to week 3
+
+Rule: file name should be 'week 1.txt', 'week 2.txt', ... 
+Rule: read lines in 'week 2.txt'
+
+Hint: with open()
+'''
+
