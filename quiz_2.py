301588279bc0de99a6013050dbd769e58d8f6add
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:40:16 -0700

    Create quiz_2.py
    Use random package to generate a random integer.
    Print using a string

---
+++ quiz_2.py
@@ -0,0 +1,15 @@
+'''
+Recently, you just created a coding study group.
+You will have 4 study sessions: 3 times online and 1 time offline.
+Make a program to meet the following offline requirements:
+
+Requirement 1: Generate a random study session date 
+Requirement 2: As every month has different number of days, use the minimum number of 28 days
+Requirement 3: Exclude 1st ~ 3rd day of every month to prepare for materials
+
+print 
+ : Your Offine Study Session Date will on 'x'th every month. 
+'''
+from random import *
+
+date = 
