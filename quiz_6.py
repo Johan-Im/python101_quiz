8c337065e5441b23fa521a00f95069bbd600a236
Johan-Im <64668374+Johan-Im@users.noreply.github.com>
2022-07-13 17:50:27 -0700

    Create quiz_6.py
    use if, elif, def

---
+++ quiz_6.py
@@ -0,0 +1,19 @@
+'''
+Make a program that calculates a standarzied weight.
+
+* Standardized weight: weight based upon individual's height
+
+Formula by gender:
+    Male: height(m) x height(m) x 22
+    Female : height(m) x height(m) x 21
+
+Rule 1: standardized weight needs to be calculated in a function/def    
+    * def name : std_weight
+    * parameter : height, gender 
+Rule 2: standardized weight needs to reflect up to the 100th decimal point
+
+Example: 
+You are a "gender", your height is "height", and your standardized weight is 67.38kg.
+'''
+
+def std_weight(height, gender): 
